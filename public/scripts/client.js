af1aad68dd1ab4f9baa2d346a151e91f9fa77a07
Sharleen Beniamin <sharleenb16@gmail.com>
2023-03-25 01:37:03 +0000

    use AJAX to fetch data from server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,30 +5,19 @@
  */
 
 $(document).ready(() => {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
+
+  const loadTweets = function() {
+    const $button = $("#submit");
+    $button.submit(function() {
+    $.ajax({
+      type: "GET", 
+      url: "/tweets", 
+      success: (response) => {
+        renderTweets(response)
       },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+    })
+  })
+}
   
   const renderTweets = function(tweets) {
     // loops through tweets
@@ -63,6 +52,6 @@ $(document).ready(() => {
     return $tweet;
   }
   
-  renderTweets(data);
+  loadTweets()
   
 });
