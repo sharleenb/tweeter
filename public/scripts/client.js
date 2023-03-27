c652ca76bc80b7ee95b4c879b78feabf04918153
Sharleen Beniamin <sharleenb16@gmail.com>
2023-03-27 23:37:31 +0000

    add escape function to prevent XSS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,8 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(() => {
 
+$(document).ready(() => {
+  
   const loadTweets = function() {
     const $button = $("#submit");
     $button.submit(function() {
@@ -29,17 +30,25 @@ $(document).ready(() => {
     // takes return value and appends it to the tweets container
   }
   
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  
   const createTweetElement = function(tweet) {
     const $tweet = `
     <article class="tweet"> 
       <header>
         <img src=${tweet["user"].avatars} alt="profile picture" />
         <h5>${tweet["user"].name}</h5> 
-        <h4>${tweet["user"].handle}</h4>
+        <h5>${tweet["user"].handle}</h5>
       </header>
-      <p> ${tweet["content"].text} </p>
+      <p> ${escape(tweet["content"].text)} </p>
+      <hr></hr>
       <footer> 
-        <p>${tweet.created_at}</p>
+        <div class="days">${timeago.format(tweet.created_at, "en_US")}</div>
         <div>
           <i class="fa-solid fa-flag"></i> 
           <i class="fa-solid fa-retweet"></i>
@@ -48,7 +57,7 @@ $(document).ready(() => {
       </footer>
     </article>
     `;
-  
+    
     return $tweet;
   }
   
